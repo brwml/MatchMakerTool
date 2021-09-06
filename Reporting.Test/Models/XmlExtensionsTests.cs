5daf9a295d2413da6477782da26efa7cf4707e5e
Bryan Wilhelm <bryan.wilhelm@live.com>
2021-09-06 00:26:49 -0400

    Add xunit tests

---
+++ Reporting.Test/Models/XmlExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace MatchMaker.Reporting.Test
+{
+    using System.Xml.Linq;
+
+    using MatchMaker.Reporting.Models;
+
+    using Xunit;
+
+    /// <summary>
+    /// Defines the <see cref="XmlExtensionsTests" />
+    /// </summary>
+    public class XmlExtensionsTests
+    {
+        /// <summary>
+        /// The VerifyConvertXmlAttributeToInt32
+        /// </summary>
+        [Fact]
+        public void VerifyConvertXmlAttributeToInt32()
+        {
+            var expected = 1;
+            var xml = new XElement("node", new XAttribute("id", expected));
+            Assert.Equal(expected, xml.GetAttribute<int>("id"));
+        }
+    }
+}
---
+++ Reporting.Test/Policies/HeadToHeadTeamRankingTests.cs
@@ -0,0 +1,42 @@
+﻿namespace MatchMaker.Reporting.Test.Policies
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    using MatchMaker.Reporting.Models;
+    using MatchMaker.Reporting.Policies;
+
+    using Newtonsoft.Json;
+
+    using Xunit;
+
+    /// <summary>
+    /// Defines the <see cref="HeadToHeadTeamRankingTests" />
+    /// </summary>
+    public class HeadToHeadTeamRankingTests
+    {
+        [Theory]
+        [MemberData(nameof(GetHeadToHeadTestCases))]
+        public void HeadToHeadTests(string fileName, IDictionary<int, int> places)
+        {
+            var results = JsonConvert.DeserializeObject<Result>(File.ReadAllText(fileName));
+            var summary = Summary.FromResult(results, new[] { new HeadToHeadTeamRankingPolicy() });
+
+            Assert.All(summary.TeamSummaries, x => Assert.Equal(x.Value.Place, places[x.Key]));
+        }
+
+        public static IEnumerable<object[]> GetHeadToHeadTestCases()
+        {
+            return new List<object[]>
+            {
+                new object[]{ @".\Policies\Data\results.0015.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 1 }, { 3, 1 } } },
+                new object[]{ @".\Policies\Data\results.0019.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 1 }, { 3, 1 }, { 4, 1 } } },
+                new object[]{ @".\Policies\Data\results.0002.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 2 } } },
+                new object[]{ @".\Policies\Data\results.0003.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 1 } } },
+                new object[]{ @".\Policies\Data\results.0035.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 1 }, { 3, 3 }, { 4, 3 }, { 5, 3 }, { 6, 6 }, { 7, 6 }, { 8, 8 } } },
+                new object[]{ @".\Policies\Data\results.0007.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 3 }, { 3, 1 } } },
+                new object[]{ @".\Policies\Data\results.0008.json", new Dictionary<int, int>{ { 1, 1 }, { 2, 2 }, { 3, 2 } } },
+            };
+        }
+    }
+}
