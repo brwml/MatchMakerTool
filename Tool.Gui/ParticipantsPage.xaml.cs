af7f8dc95618ff1b011c5cb9a04b7b9d04ca00ee
Bryan Wilhelm <bryan.wilhelm@live.com>
2021-11-11 22:58:52 -0500

    Create GUI tabs

---
+++ Tool.Gui/ParticipantsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MatchMaker.Tool.Gui;
+
+using System.Windows.Controls;
+
+/// <summary>
+/// Interaction logic for ParticipantsPage.xaml
+/// </summary>
+public partial class ParticipantsPage : Page
+{
+    public ParticipantsPage()
+    {
+        this.InitializeComponent();
+    }
+}
