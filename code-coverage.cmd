537fa0f7ff615e39c38371435adfa820f69f4adf
Bryan Wilhelm <bryan.wilhelm@live.com>
2021-11-17 21:20:48 -0500

    Remove code coverage script

--- code-coverage.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-dotnet test --collect:"XPlat Code Coverage" --results-directory .\coverage
-
-reportgenerator -reports:".\coverage\*\coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
