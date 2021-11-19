6526bc30778a3588eb2b6046e432fa959b6a0557
Bryan Wilhelm <bryan.wilhelm@live.com>
2021-11-19 00:02:11 -0500

    Make quizzer result properties read-only

--- Reporting/Models/QuizzerResult.cs
+++ Reporting/Models/QuizzerResult.cs
@@ -14,22 +14,35 @@ using Ardalis.GuardClauses;
 public class QuizzerResult
 {
     /// <summary>
+    /// Initializes a new instance of the <see cref="QuizzerResult"/> class.
+    /// </summary>
+    /// <param name="id">The quizzer identifier</param>
+    /// <param name="score">The quizzer score</param>
+    /// <param name="errors">The quizzer errors</param>
+    private QuizzerResult(int id, int score, int errors)
+    {
+        this.QuizzerId = id;
+        this.Score = score;
+        this.Errors = errors;
+    }
+
+    /// <summary>
     /// Gets or sets the Errors
     /// </summary>
     [DataMember]
-    public int Errors { get; set; }
+    public int Errors { get; }
 
     /// <summary>
     /// Gets or sets the quizzer identifier
     /// </summary>
     [DataMember]
-    public int QuizzerId { get; set; }
+    public int QuizzerId { get; }
 
     /// <summary>
     /// Gets or sets the Score
     /// </summary>
     [DataMember]
-    public int Score { get; set; }
+    public int Score { get; }
 
     /// <summary>
     /// Creates a <see cref="QuizzerResult"/> instance from an XML element.
@@ -40,12 +53,10 @@ public class QuizzerResult
     {
         Guard.Against.Null(xml, nameof(xml));
 
-        return new QuizzerResult
-        {
-            QuizzerId = xml.GetAttribute<int>("id"),
-            Score = xml.GetAttribute<int>("score"),
-            Errors = xml.GetAttribute<int>("errors")
-        };
+        return new QuizzerResult(
+            xml.GetAttribute<int>("id"),
+            xml.GetAttribute<int>("score"),
+            xml.GetAttribute<int>("errors"));
     }
 
     /// <summary>
