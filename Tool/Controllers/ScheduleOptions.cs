99d3bcf81b1b3adb448463f4f3121f52f616b3f8
Bryan Wilhelm <bryan.wilhelm@live.com>
2023-05-14 12:23:49 -0400

    Rename scheduling command line options

--- Tool/Controllers/ScheduleOptions.cs
+++ Tool/Controllers/ScheduleOptions.cs
@@ -21,24 +21,33 @@ internal class ScheduleOptions : BaseOptions
     /// <param name="sourceSchedule">The source schedule.</param>
     /// <param name="verbose">If set to <c>true</c>, then emit verbose output.</param>
     public ScheduleOptions(
-        string outputSchedule,
+        string inputSchedulePath,
+        string outputSchedulePath,
         int rooms,
         ScheduleType scheduleType,
-        string sourceSchedule,
         bool verbose)
         : base(verbose)
     {
-        this.OutputSchedule = Guard.Against.NullOrWhiteSpace(outputSchedule);
+        this.InputSchedulePath = Guard.Against.NullOrWhiteSpace(inputSchedulePath);
+        this.OutputSchedulePath = Guard.Against.NullOrWhiteSpace(outputSchedulePath);
         this.Rooms = rooms;
         this.ScheduleType = scheduleType;
-        this.SourceSchedule = Guard.Against.NullOrWhiteSpace(sourceSchedule);
+    }
+
+    /// <summary>
+    /// Gets or sets the source schedule file path
+    /// </summary>
+    [Option('i', Required = true, HelpText = "The source schedule file path")]
+    public string InputSchedulePath
+    {
+        get;
     }
 
     /// <summary>
     /// Gets or sets the output schedule file path
     /// </summary>
     [Option('o', Required = true, HelpText = "The output schedule file path")]
-    public string OutputSchedule
+    public string OutputSchedulePath
     {
         get;
     }
@@ -57,15 +66,6 @@ internal class ScheduleOptions : BaseOptions
     /// </summary>
     [Option('t', Default = ScheduleType.RoundRobin, Required = false, HelpText = "The schedule type. The only option is RoundRobin.")]
     public ScheduleType ScheduleType
-    {
-        get;
-    }
-
-    /// <summary>
-    /// Gets or sets the source schedule file path
-    /// </summary>
-    [Option('i', Required = true, HelpText = "The source schedule file path")]
-    public string SourceSchedule
     {
         get;
     }
