9f36e98ffa4123e02a9b8bec39f6cc13b1e5c1ff
Bryan Wilhelm <bryan.wilhelm@live.com>
2023-07-02 10:53:12 -0400

    Add scheduling implementation

--- Tool/Controllers/SchedulingController.cs
+++ Tool/Controllers/SchedulingController.cs
@@ -1,6 +1,11 @@
 ﻿namespace MatchMaker.Tool.Controllers;
 
+using System.Xml.Linq;
+
 using Ardalis.GuardClauses;
+
+using MatchMaker.Reporting.Models;
+using MatchMaker.Scheduling;
 
 /// <summary>
 /// Defines the <see cref="SchedulingController" />
@@ -16,6 +21,35 @@ internal class SchedulingController : IProcessController<ScheduleOptions>
     {
         Guard.Against.Null(options);
 
+        if (options.ScheduleType == ScheduleType.RoundRobin)
+        {
+            RoundRobinTournament
+                .Create(LoadInputSchedule(options.InputSchedulePath), options.Rooms)
+                .ToXml()
+                .Save(options.OutputSchedulePath);
+        }
+
         return true;
+    }
+
+    /// <summary>
+    /// Loads the input schedule.
+    /// </summary>
+    /// <param name="schedulePath">The schedule path.</param>
+    /// <returns>The schedule instance</returns>
+    private static Schedule LoadInputSchedule(string schedulePath)
+    {
+        var scheduleName = Path.GetFileNameWithoutExtension(schedulePath);
+        return Schedule.FromXml(LoadXml(schedulePath), scheduleName);
+    }
+
+    /// <summary>
+    /// Loads the XML document.
+    /// </summary>
+    /// <param name="schedulePath">The schedule path.</param>
+    /// <returns>The XML document</returns>
+    private static XDocument LoadXml(string schedulePath)
+    {
+        return XDocument.Load(schedulePath);
     }
 }
