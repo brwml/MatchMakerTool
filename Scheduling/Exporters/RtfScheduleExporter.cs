731115e27044936792d3ceba63a1918acbeed821
Bryan Wilhelm <bryan.wilhelm@live.com>
2023-09-02 11:45:07 -0400

    Add RTF schedule exporter stub

---
+++ Scheduling/Exporters/RtfScheduleExporter.cs
@@ -0,0 +1,13 @@
+﻿namespace MatchMaker.Scheduling.Exporters;
+
+using System;
+
+using MatchMaker.Models;
+
+public class RtfScheduleExporter : IScheduleExporter
+{
+    public void Export(Schedule schedule, string folder)
+    {
+        throw new NotImplementedException();
+    }
+}
